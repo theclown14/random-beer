6aaea7bc9a47e666c3c1c776325f2a8c9dd65752
Danil Fotin <d.fotin@coffeestudio.ru>
2023-02-07 19:44:34 +0700

    get  beer data

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="./assets/beer.jpg" alt="">
+    <BeerData />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BeerData from './components/BeerData.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BeerData
   }
 }
 </script>
@@ -24,5 +24,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+img {
+  width: 300px;
+  height: 200px;
 }
 </style>
---
+++ src/components/BeerData.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="">
+    <h1>Пиво brand</h1>
+    <h2>{{ beer.brand }}</h2>
+    <h1>Пиво name</h1>
+    <h2>{{ beer.name }}</h2>
+    <h1>Пиво style</h1>
+    <h2>{{ beer.style }}</h2>
+    <h1>Пиво alcohol</h1>
+    <h2>{{ beer.alcohol }}</h2>
+    <span v-if="beer.alcohol > '7.0%'">hight</span>
+    <span v-else>low</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      beer: ""
+    }
+  },
+  mounted() {
+    const BeerData = async () => {
+      let url = 'https://random-data-api.com/api/beer/random_beer';
+      let response = await fetch(url);
+      this.beer = await response.json();
+    }
+    BeerData();
+  }
+}
+</script>
+
+
+<style scoped>
+h1 {
+  color: salmon;
+}
+</style>
